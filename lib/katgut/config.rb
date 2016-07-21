5d92d5ded41f27bae8530bfd9201c2ebd7a5bfad
HAMADA Kazuki <kazuki-hamada@cookpad.com>
2016-07-21 16:21:33 +0900

    Add config option to enable redirection count

--- lib/katgut/config.rb
+++ lib/katgut/config.rb
@@ -1,11 +1,14 @@
 module Katgut
   class Config
     include ActiveSupport::Configurable
-    config_accessor :after_redirection_callback, :fall_back_path
+
+    config_accessor :after_redirection_callback, :fall_back_path,
+      :enable_redirection_count
 
     configure do |config|
       config.after_redirection_callback = nil
       config.fall_back_path = '/'
+      config.enable_redirection_count = false
     end
   end
 end
